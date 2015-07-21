db976e8aa5f2c731dd3d2c590b4dad7cbb822991
Michael Stalker <mtstalker@gmail.com>
2015-07-21 16:20:19 -0400

    Add WebSocket support with Sente

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
                  [compojure "1.3.1"]
                  [hiccup "1.0.5"]
                  [ring "1.4.0"]
+                 [com.taoensso/sente "1.5.0"]
                  [ring/ring-defaults "0.1.5"]
                  [http-kit "2.0.0"]]
 
--- src/planning_poker/routes.clj
+++ src/planning_poker/routes.clj
@@ -7,15 +7,30 @@
             [compojure.route :as route]
             [compojure.handler :refer [site]]
             [ring.middleware.reload :as reload]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [taoensso.sente :as sente]
+            [taoensso.sente.server-adapters.http-kit :refer (sente-web-server-adapter)]))
+
+(let [{:keys [ch-recv send-fn ajax-post-fn ajax-get-or-ws-handshake-fn connected-uids]}
+      (sente/make-channel-socket! sente-web-server-adapter {})]
+  (def ring-ajax-post ajax-post-fn)
+  (def ring-ajax-get-or-ws-handshake ajax-get-or-ws-handshake-fn)
+  (def ch-chsk ch-recv) ; ChannelSocket's receive channel
+  (def chsk-send! send-fn) ; ChannelSocket's send API fn
+  (def connected-uids connected-uids)) ; Watchable, read-only atom
 
 (defroutes app-routes
   (GET "/" [] (index-page))
   (GET "/:game-number" [] (voting-page))
+  (GET  "/chsk" req (ring-ajax-get-or-ws-handshake req))
+  (POST "/chsk" req (ring-ajax-post req))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+      (wrap-defaults site-defaults)
+      ring.middleware.keyword-params/wrap-keyword-params
+      ring.middleware.params/wrap-params))
 
 ;; TODO read a config variable from command line, env, or file?
 (defn in-dev? [args] true)
