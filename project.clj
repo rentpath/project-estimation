8eea66dc72454892247cb7ad6fc5a19965ab5737
Michael Stalker <mtstalker@gmail.com>
2015-07-21 15:54:51 -0400

    Switch to http-kit
    This will give us the ability to use the WebSocket protocol.

--- project.clj
+++ project.clj
@@ -2,12 +2,17 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
+
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
                  [hiccup "1.0.5"]
-                 [ring/ring-defaults "0.1.2"]]
-  :plugins [[lein-ring "0.8.13"]]
-  :ring {:handler planning-poker.routes/app}
+                 [ring "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]
+                 [http-kit "2.0.0"]]
+
   :profiles
+
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+                        [ring-mock "0.1.5"]]}}
+
+  :main planning-poker.routes)
--- src/planning_poker/routes.clj
+++ src/planning_poker/routes.clj
@@ -1,8 +1,12 @@
 (ns planning-poker.routes
   (:use planning-poker.views
-        [hiccup.middleware :only (wrap-base-url)])
+        [hiccup.middleware :only (wrap-base-url)]
+        [org.httpkit.server :only [run-server]])
+
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [compojure.handler :refer [site]]
+            [ring.middleware.reload :as reload]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
@@ -12,3 +16,17 @@
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
+;; TODO read a config variable from command line, env, or file?
+(defn in-dev? [args] true)
+
+;; Entry point. `lein run` will pick up and start from here
+(defn -main [& args]
+  (let [handler (if (in-dev? args)
+                  (reload/wrap-reload (site #'app))
+                  (site app))]
+    (run-server handler {:port 8080})))
+
+;; run-server returns a function that stops the server
+; (let [server (run-server app options)]
+;   (server))
