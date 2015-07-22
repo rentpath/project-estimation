80942d8cf0d6a026e1a852d437268da0e7d42197
cbui <cbui@rentpath.com>
2015-07-22 17:04:30 -0500

    Push to M.S.

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
                  [org.clojure/clojurescript "0.0-3308"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [hiccup "1.0.5"]
                  [ring "1.4.0"]
                  [com.taoensso/sente "1.5.0"]
