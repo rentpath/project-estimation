d4e6285b6ef854ada5ece53c56fe6f04248f60c2
Michael Stalker <mtstalker@gmail.com>
2015-07-23 21:01:00 -0400

    Introduce players atom
    Keep track of all players and their names. Also keep track of the
    players' URI path. I will remove that part soon, and will add it back
    later if there's need.

--- src/planning_poker/message_handler.clj
+++ src/planning_poker/message_handler.clj
@@ -1,13 +1,19 @@
 (ns planning-poker.message-handler
   (:require [taoensso.sente :as sente]
+            [clojure.pprint]
             [taoensso.sente.server-adapters.http-kit :refer (sente-web-server-adapter)]))
 
+(defonce players (atom {}))
+
+(defn player-id
+  [ring-request]
+  (-> ring-request
+      :cookies
+      (get "ring-session")
+      :value))
+
 (let [{:keys [ch-recv send-fn ajax-post-fn ajax-get-or-ws-handshake-fn connected-uids]}
-      (sente/make-channel-socket! sente-web-server-adapter {:user-id-fn (fn [ring-req]
-                                                                          (-> ring-req
-                                                                              :cookies
-                                                                              (get "ring-session")
-                                                                              :value))})]
+      (sente/make-channel-socket! sente-web-server-adapter {:user-id-fn player-id})]
   (def ring-ajax-post ajax-post-fn)
   (def ring-ajax-get-or-ws-handshake ajax-get-or-ws-handshake-fn)
   (def ch-chsk ch-recv) ; ChannelSocket's receive channel
@@ -34,20 +40,19 @@
       (?reply-fn {:umatched-as-echoed-from-from-server event}))))
 
 (defmethod message-handler :chsk/ws-ping
-  [evt]
+  [event]
   :no-op)
 
 (defmethod message-handler :planning-poker.core/user-joined-session
-  [evt]
-  (println "User joined: " evt)
-  (println (str @connected-uids))
-  (notify-all @connected-uids))
+  [{:as event-message :keys [?data]}]
+  (let [ring-request (:ring-req event-message)]
+    (clojure.pprint/pprint event-message)
+    (swap! players assoc (player-id ring-request) ?data)
+    (notify-all @connected-uids)))
 
 ;; Add your (defmethod message-handler <id> [event-message] <body>)s here...
 
 (sente/start-chsk-router! ch-chsk message-handler*)
-
-(defonce players (atom {}))
 
 ; (notify-all @connected-uids)
 ;; run-server returns a function that stops the server
@@ -59,4 +64,6 @@
 (comment
   (chsk-send! "ebf214e7-d41a-4990-acad-c60675d30a30" [::user-joined-session {:name "Michael"}])
   (chsk-send! :sente/all-users-without-uid [::user-estimated {:a :data}])
+  (player-id {:cookies {"ring-session" {:value "abcd"}}})
+  (clojure.pprint/pprint @players)
   )
