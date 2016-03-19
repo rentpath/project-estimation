f2798ac78f688136dbffb756844702b36db2093c
Michael Stalker <mtstalker@gmail.com>
2016-03-19 16:35:28 -0400

    Style played cards

--- src/planning_poker/client/core.cljs
+++ src/planning_poker/client/core.cljs
@@ -85,10 +85,11 @@
   [player]
   [:li.player
    [:span.name (:name player)]
-   [:span.estimate (cond
-                     (:show-estimate? player) (:estimate player)
-                     (:estimate player) "Done"
-                     :else "Waiting")]])
+   [:span.estimate {:class (cond
+                             (:show-estimate? player) :estimate-show
+                             (:estimate player) :estimate-hide
+                             :else :estimate-waiting)}
+    (when (:show-estimate? player) (:estimate player))]])
 
 (defn players-component
   [players]
