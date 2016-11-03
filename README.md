0bbdd4bb82ce5adc7e8117867858ee47d3afd8a7
Michael Stalker <mtstalker@gmail.com>
2016-11-03 18:48:21 -0400

    Add copyright for Planning Poker
    Mountain Goat Software coined the phrase and the card sequence. I’m
    giving credit where credit is due.

--- README.md
+++ README.md
@@ -26,6 +26,8 @@ Then, load http://localhost:8080 in your web browser.
 
 If you use ngrok, make sure your web server configuration allows you to access the app at http://127.0.0.1:8080.
 
-## License
+## Licenses
+PLANNING POKER ® is a registered trademark of Mountain Goat Software, LLC
+Sequence of values is (C) Mountain Goat Software, LLC
 
 Copyright © 2015
--- src/planning_poker/views.clj
+++ src/planning_poker/views.clj
@@ -12,7 +12,10 @@
      [:a.new-game {:href (str "/" (java.util.UUID/randomUUID))} "Start a Game"]
 
      [:h2 "Join Existing Game"]
-     [:p "Ask someone on your team for the URL of their planning poker session."]]]))
+     [:p "Ask someone on your team for the URL of their planning poker session."]
+     [:footer.copyright
+      [:p "PLANNING POKER ® is a registered trademark of Mountain Goat Software, LLC"]
+      [:p "Sequence of values is (C) Mountain Goat Software, LLC"]]]]))
 
 (defn voting-page []
   (html5
